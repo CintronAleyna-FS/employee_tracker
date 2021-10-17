eb7fb19a7af1fb6a229b8a071726fdf0ba6c1df1
CintronAleyna-FS <88294142+CintronAleyna-FS@users.noreply.github.com>
2021-10-17 14:42:54 -0500

    created all classes needed in JS.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,9 +1,44 @@
+// Super / Base class
+class Employee{
+    constructor(name, age, annualSalary){
+        this.name = name;
+        this.age = age;
+        this.annualSalary = annualSalary;
+    }
+}
+
+class PartTime extends Employee{
+
+    constructor(payRate, hours, type){
+        super();
+        this.payRate = payRate;
+        this.hours = hours;
+        this.employeeType = type;
+
+    }
+
+    calculatePay(){
+
+    }
+}
+
+// An employee who works 40+ hours
+class FullTime extends Employee{
+
+    constructor(payRate, type){
+        super();
+        this.payRate = payRate;
+        this.employeeType = type;
+
+    }
+    calculatePay(){
+
+    }
+}
+
 class Main{
 
 }
-
-
-
 
 // IIFE
 (()=>{
